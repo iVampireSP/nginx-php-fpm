9ce1613b23f466b84e550ef4611b06d6a4f90253
root <root@localhost.localdomain>
2024-06-21 17:41:14 +0800

    update

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ fi
 if [ $tag != 'latest' ]
 then
   echo 'Build from tag'
-  docker build -f src/docker/${tag}/Dockerfile -t jkaninda/nginx-php-fpm:$tag .
+  docker build -f src/docker/${tag}/Dockerfile -t registry.leafdev.top/packaged/nginx-php-fpm-docker:$tag .
 else
  echo 'Build latest'
- docker build -f src/docker/8.3/Dockerfile -t jkaninda/nginx-php-fpm:$tag .
+ docker build -f src/docker/8.3/Dockerfile -t registry.leafdev.top/packaged/nginx-php-fpm-docker:$tag .
  
 fi
 docker compose up -d --force-recreate
