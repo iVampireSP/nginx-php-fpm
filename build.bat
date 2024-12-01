6058fd288d738e6b915bc02f5a82c7949256b736
ivamp <im@ivampiresp.com>
2024-12-01 23:39:44 +0800

    update

--- build.bat
+++ build.bat
@@ -2,16 +2,20 @@
 
 
 echo Building 7.4...
-docker build -f src/docker/7.4/Dockerfile -t registry.leafdev.top/packaged/nginx-php-fpm-docker:7.4 .
-docker push registry.leafdev.top/packaged/nginx-php-fpm-docker:7.4
+docker build -f src/docker/7.4/Dockerfile -t leafdev.top/packaged/nginx-php-fpm-docker:7.4 .
+docker push leafdev.top/packaged/nginx-php-fpm-docker:7.4
 
 echo Building 8.0...
-docker build -f src/docker/8.0/Dockerfile -t registry.leafdev.top/packaged/nginx-php-fpm-docker:8.0 .
-docker push registry.leafdev.top/packaged/nginx-php-fpm-docker:8.0
+docker build -f src/docker/8.0/Dockerfile -t leafdev.top/packaged/nginx-php-fpm-docker:8.0 .
+docker push leafdev.top/packaged/nginx-php-fpm-docker:8.0
 
 echo Building 8.0-swoole-loader...
-docker build -f src/docker/8.0-swoole-loader/Dockerfile -t registry.leafdev.top/packaged/nginx-php-fpm-docker:8.0-swoole-loader .
-docker push registry.leafdev.top/packaged/nginx-php-fpm-docker:8.0-swoole-loader
+docker build -f src/docker/8.0-swoole-loader/Dockerfile -t leafdev.top/packaged/nginx-php-fpm-docker:8.0-swoole-loader .
+docker push leafdev.top/packaged/nginx-php-fpm-docker:8.0-swoole-loader
+
+echo Building 8.2...
+docker build -f src/docker/8.2/Dockerfile -t leafdev.top/packaged/nginx-php-fpm-docker:8.2 .
+docker push leafdev.top/packaged/nginx-php-fpm-docker:8.2
 
 
 echo Finished.
